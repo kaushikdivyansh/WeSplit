474071c8b72af8afd51e67bf6200b91499d51c8c
Divyansh Kaushik <kaushik.divyansh@icloud.com>
2023-06-22 16:06:23 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Divyansh Kaushik on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
